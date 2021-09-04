ac8a5675da72aa7c3fb530540f243c28691a05c8
vitorhugocamisa10 <vitorhugomf@outlook.com.br>
2021-09-04 00:04:38 +0000

    (Aula 1)

---
+++ revisao -js/propriedades-abreviadas.js
@@ -0,0 +1,25 @@
+/*let infoUsuario = {
+    usuario: 'Cleitin',
+    nomeCompleto: 'Cleitin Silva Pinto',
+    grupo: 'alunos',
+    senha: '6984'
+}
+*/
+
+let usuario = 'Gabigol'
+let nome = 'Gabigol Silva Santos'
+let grupo = 'alunos'
+let password = '9621'
+
+// Abreviando propriedades
+let infoUsuario = {
+    usuario,  
+    nomeCompleto: nome,
+    grupo,
+    senha: password
+}
+
+console.log(infoUsuario)
+
+// Exibindo o conteúdo da variável 'password' para fins de depuração
+console.log(password)
